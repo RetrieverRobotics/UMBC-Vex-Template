d37e6d0b4f3c31acdd0911db52a369e0a8c96937
jkieltyka15 <jmkielty@mtu.edu>
2023-01-20 19:49:23 -0500

    UMBC #7: Implemented ControllerInput class.

--- include/umbc/controllerinput.hpp
+++ include/umbc/controllerinput.hpp
@@ -1,5 +1,5 @@
 /**
- * \file umbc/vcontroller.hpp
+ * \file umbc/controllerinput.hpp
  *
  * Contains the prototype for the ControllerInput. ControllerInput
  * stores controller values with a minimal memory footprint.
@@ -15,6 +15,11 @@
 using namespace pros;
 
 namespace umbc {
+typedef enum {
+    E_CONTROLLER_ANALOG_MIN = -127,
+    E_CONTROLLER_ANALOG_MAX = 127
+} controller_analog_e_limit_t;
+
 class ControllerInput {
 
     private:
@@ -64,6 +69,34 @@ class ControllerInput {
 	 * \return The value of the digital input: 1 or 0
 	 */
     std::int32_t get_digital(controller_digital_e_t button);
+
+    /**
+	 * Sets the value of an analog input (joystick).
+	 *
+	 * \param channel
+	 * 			  The analog input value to set.
+	 * 			  Must be one of ANALOG_LEFT_X, ANALOG_LEFT_Y, ANALOG_RIGHT_X,
+	 *        ANALOG_RIGHT_Y
+	 *
+	 * \param value
+     *          The value for the analog input. Must be [-127, 127]. Values
+     *          lower than -127 will be converted to -127, and values higher
+     *          than 127 will be converted to 127.
+	 */
+    void set_analog(controller_analog_e_t channel, std::int32_t value);
+
+    /**
+	 * Sets the value of a digital input (button).
+	 *
+	 * \param button
+	 *      The digital input value to set. Must be one of
+	 *      DIGITAL_{RIGHT,DOWN,LEFT,UP,A,B,Y,X,R1,R2,L1,L2}
+	 *
+	 * \param value
+     *      The value for the digital input. Must be 0 or 1. Non-zero numbers
+     *      will be converted to 1.
+	 */
+    void set_digital(controller_digital_e_t button, std::int32_t value);
 
 };
 }
