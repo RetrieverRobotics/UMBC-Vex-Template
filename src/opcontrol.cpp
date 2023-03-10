4a04361271b1bbcc12931ca31588e7fc43eef0ae
jkieltyka15 <jmkielty@mtu.edu>
2023-03-10 09:08:06 -0500

    UMBC #32: Implemented main.cpp and added seperate file for user defined opcontrol.

---
+++ src/opcontrol.cpp
@@ -0,0 +1,27 @@
+/**
+ * \file opcontrol.cpp
+ *
+ * Contains user implemented opcontrol. User must use the
+ * parameters to the opcontrol function when referencing
+ * the master V5 controller or partner V5 controller.
+ */
+
+#include "api.h"
+#include "umbc.h"
+
+#include <cstdint>
+
+using namespace pros;
+using namespace umbc;
+using namespace std;
+
+void umbc::Robot::opcontrol(pros::Controller controller_master, pros::Controller controller_partner) {
+
+    // initialize motors and sensors
+
+    while(1) {
+
+        // implement opcontrols
+
+    }
+}
