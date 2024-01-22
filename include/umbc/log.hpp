ad919e497af45e4a13eb9cc791ec1fc3bb79ecb8
jkieltyka15 <jmkielty@mtu.edu>
2024-01-21 19:39:11 -0500

    UMBC #66: Logging can now easily be toggled on or off.

--- include/umbc/log.hpp
+++ include/umbc/log.hpp
@@ -1,7 +1,8 @@
 /**
  * \file umbc/log.hpp
  *
- * Contains defintions for logging to the pros terminal
+ * Contains definitions for logging to the pros terminal. To disable
+ * commenting, ensure LOG is not defined.
  */
 
 #ifndef _UMBC_LOG_HPP_
@@ -9,8 +10,17 @@
 
 #include <iostream>
 
+#define LOG // comment out this line to disable logging
+
+#ifdef LOG
 #define INFO(str) cout << "INFO: " << str << endl;
 #define WARN(str) cout << "WARN: " << str << endl;
 #define ERROR(str) cerr << "ERROR: " << str << endl;
+
+#else
+#define INFO(str)
+#define WARN(str)
+#define ERROR(str)
+#endif
 
 #endif // _UMBC_LOG_HPP_
