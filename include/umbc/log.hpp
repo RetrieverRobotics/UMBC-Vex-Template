94f6a1444fa91e606ea8f2243c4f042e5b7db689
jkieltyka15 <jmkielty@mtu.edu>
2024-01-21 19:44:49 -0500

    UMBC #66 - Added mechanism for disabling logging when building via a define in log.hpp.

--- include/umbc/log.hpp
+++ include/umbc/log.hpp
@@ -9,8 +9,17 @@
 
 #include <iostream>
 
+#define LOG // comment out this line to disable logging
+
+#ifdef LOG
 #define INFO(str) cout << "INFO: " << str << endl;
 #define WARN(str) cout << "WARN: " << str << endl;
 #define ERROR(str) cerr << "ERROR: " << str << endl;
+
+#else
+#define INFO(str)
+#define WARN(str)
+#define ERROR(str)
+#endif
 
 #endif // _UMBC_LOG_HPP_
