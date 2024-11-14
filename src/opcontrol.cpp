158be5e7134f72cc57ce7581072b755e4b526429
Bluish1251 <cm6of6@gmail.com>
2024-11-14 16:47:45 -0500

    pushed to wrong repo, undoing changed

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -30,24 +30,12 @@ void umbc::Robot::opcontrol() {
     umbc::Controller* controller_partner = this->controller_partner;
 
     // initialize motors and sensors
-    pros::Motor drive_left_motor = pros::Motor(LEFT_MOTOR_PORT);
-    pros::MotorGroup drive_left = pros::MotorGroup(vector<pros::Motor>{drive_left_motor});
 
-    pros::Motor drive_right_motor = pros::Motor(RIGHT_MOTOR_PORT);
-    pros::MotorGroup drive_right = pros::MotorGroup(vector<pros::Motor>{drive_right_motor});
 
     while(1) {
-        int32_t arcade_y = controller_master->get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
-        int32_t arcade_x = controller_master->get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
+
         // implement opcontrols
-        int32_t drive_left_velocity = (int32_t)(((double)(arcade_y + arcade_x) / (double)E_CONTROLLER_ANALOG_MAX)
-                                        * MOTOR_GREEN_GEAR_MULTIPLIER);
 
-        int32_t drive_right_velocity = (int32_t)(((double)(arcade_y - arcade_x) / (double)E_CONTROLLER_ANALOG_MAX)
-                                        * MOTOR_GREEN_GEAR_MULTIPLIER);
-
-        drive_left.move_velocity(drive_left_velocity);
-        drive_right.move_velocity(drive_right_velocity);
         // required loop delay (do not edit)
         pros::Task::delay(this->opcontrol_delay_ms);
     }
